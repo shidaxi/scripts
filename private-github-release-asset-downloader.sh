4b32da95aa9f301da37324cac9ec7715c41b63fd
Yoda <shixi.gs@gmail.com>
2024-03-27 22:06:31 +0800

    update

---
+++ private-github-release-asset-downloader.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+repo="$1"
+tag="$2"
+asset_name="$3"
+
+help() {
+  echo 'bash -c $(curl -fsSL https://shuai.dev/scripts/private-github-release-asset-downloader.sh) cli/cli v2.46.0 gh_2.46.0_linux_amd64.tar.gz'
+}
+
+if [ -z "$repo" ] -o [ -z "$tag" ] -o [ -z "${asset_name}" ]; then
+    help
+fi
+
+[ -z "${GITHUB_TOKEN}" ] && { echo "GITHUB_TOKEN not set"; exit 1; }
+
+asset_url=$(curl -sSL \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN} "\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/initia-labs/movevm/releases \
+  | jq '.[] | select(.tag_name=="'$tag'") | .assets[] | select(.name=="'${asset_name}'") | .url')
+
+curl -sSL ${asset_url} -o $asset_name
